82d4b8e7fe22861f15d01b35a99336e5c3566b63
Jose Villalobos <jorovipe@gmail.com>
2021-07-11 13:26:04 -0500

    Implemented draw vector utilities.

--- Assets/Scripts/Math/Utils/DrawVector2D.cs
+++ Assets/Scripts/Math/Utils/DrawVector2D.cs
@@ -6,10 +6,44 @@ namespace MyMath
 {
     public static class DrawVector2D
     {
-        public static void Draw(Vector2D vector)
+
+        public static void DoDraw(Vector2D vector, Color color)
         {
-            //Debug.DrawLine(Vector3.zero, new Vector3(vec))
+            Debug.DrawLine(Vector3.zero, vector, color);
+        }
+
+        public static void DoDraw(Vector2D origin, Vector2D vector, Color color)
+        {
+            Debug.DrawLine(origin, origin + vector, color);
+        }
+
+        /// <summary>
+        /// This is a extension method can be used like this with an instance
+        /// of a Vector2D:
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="color"></param>
+        public static void Draw(this Vector2D vector, Color color)
+        {
+            DoDraw(vector, color);
+        }
+
+        /// <summary>
+        /// This is to allow the draw of the vector but with a origin different than
+        /// (0, 0)
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="origin"></param>
+        /// <param name="color"></param>
+        /// <example>
+        /// <code>
+        /// Vector2D myVector = new Vector2D(2, 2);
+        /// myVector.Draw(origin, color);
+        /// </code>
+        /// </example>
+        public static void Draw(this Vector2D vector, Vector2D origin, Color color)
+        {
+            DoDraw(origin, vector, color);
         }
     }
-
 }
--- Assets/Scripts/UsingVector.cs
+++ Assets/Scripts/UsingVector.cs
@@ -5,35 +5,24 @@ using MyMath;
 
 public class UsingVector : MonoBehaviour
 {
-    //public Vector2D vectorA;
-    //public Vector2D vectorB;
+    [Header("Origin")]
+    [SerializeField]
+    private Vector2D origin = new Vector2D(0, 0);
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Vector2D vectorA = new Vector2D(10, 10);
-        //Vector2D vectorB = new Vector2D(10, 10);
-        //Debug.Log(vectorA);
-        //Vector2D vectorResult = vectorA.Sum(vectorB);
-        //Debug.Log(vectorA);
-        //Debug.Log(vectorResult);
-        //Debug.Log(vectorA + vectorB);
+    [Header("Vector")]
+    [SerializeField]
+    private Color color = Color.green;
 
-        //Vector2 vectorA = new Vector2(10, 10);
-        //Vector2 vectorB = new Vector2(10, 10);
-        //Debug.Log(vectorA);
-        //Vector2 vectorResult = vectorA + vectorB;
-        //Debug.Log(vectorA);
-        //Debug.Log(vectorResult);
-
-        //Vector2 va = new Vector2(1, 1);
-        //Vector2.
-        //Debug.Log()
-    }
+    [SerializeField]
+    private Vector2D vector = new Vector2D(10, 10);
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Note that we are calling a extension method.
+        // It's equivalent to do the next:
+        // DrawVector2D.DoDraw(origin, vector, color);
+        // The extension methods are just syntax sugar for static methods.
+        vector.Draw(origin, color);
     }
 }
